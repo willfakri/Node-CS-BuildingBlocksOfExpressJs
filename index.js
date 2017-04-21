e27cabfc85ac6f4c5d928f253f7c56f8da4a277c
Willian <willfakri@gmail.com>
2017-04-21 12:49:56 -0300

    Lição 1 - Adicionado retorno json

--- index.js
+++ index.js
@@ -9,6 +9,11 @@ app.get('/redirecionar', function(request, response){
     response.redirect(301, '/');
 });
 
+app.get('/json', function(request, response){
+    var array = ['bejeto1', 'bejeto2'];
+    response.json(array);
+});
+
 app.listen(3000, function() {
     console.log('Listening on port 3000');
 });
